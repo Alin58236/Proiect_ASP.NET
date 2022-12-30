b1b1db3c7cd829c11b032ee3ca19e2b8f66dda43
Stoia <alin58236@gmail.com>
2022-12-30 02:41:22 +0200

    Modified DB To contain Employee Email

---
+++ Migrations/20221230004010_CreateAngajatEmail.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace beerT.Migrations
+{
+    public partial class CreateAngajatEmail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Angajat",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Angajat");
+        }
+    }
+}
---
+++ Migrations/LibraryIdentity/20221230004030_CreateAngajatEmail.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace beerT.Migrations.LibraryIdentity
+{
+    public partial class CreateAngajatEmail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
